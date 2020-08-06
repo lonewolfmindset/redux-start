f28edf14de27a6e1dcee0c9ec2ab85cb9b5fe769
lonewolfmindset <lonewolfmindset7@outlook.com>
2020-08-06 18:27:05 -0400

    Higher order functions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+setTimeout(() => console.log("Hello"), 1000);
+
+let numbers = [1, 2, 3];
+numbers.map(number => number * 2);
+
+function greet(fn) {
+    console.log(fn());
+}
+
 function sayHello() {
     return function() {
         return "Hello World";
     }
 }
-
-let fn = sayHello();
-let message = fn();
