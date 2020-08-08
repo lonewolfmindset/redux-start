77e9b31fff05488544013b8b2400720f69234671
lonewolfmindset <lonewolfmindset7@outlook.com>
2020-08-07 20:46:36 -0400

    Immer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import { Map } from 'immutable';
+import { produce } from 'immer';
 
-let book = Map({title: "Harry Potter" });
+let book = {title: "Harry Potter" };
 
 function publish(book) {
-    return book.set("isPublished", true);
+    return produce(book, draftBook => {
+        draftBook.isPublished = true;
+    });
 }
 
-book = publish(book);
+let updated = publish(book);
 
-console.log(book.get("title"));
-console.log(book.toJS());
+console.log(book);
+console.log(updated);
