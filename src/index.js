c5e57584b19a928fffadb6e69c2e670038f1c13f
lonewolfmindset <lonewolfmindset7@outlook.com>
2020-08-07 20:19:13 -0400

    Updating arrays

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
-const person = { 
-    name: "John",
-    address: {
-        country: "USA",
-        city: "San Francisco"
-    } 
-};
+const numbers = [1, 2, 3];
 
-const updated = {
-    ...person,
-    address: {
-        ...person.address,
-        city: "New York"
-    },
-    name: "Bob"
-};
-updated.address.city = "New York";
-console.log(person);
+// Adding
+const index = numbers.indexOf(2);
+const added = [
+    ...numbers.slice(0, index),
+    4,
+    ...numbers.slice(index)
+];
+
+// Removing
+const removed = numbers.filter(n => n !== 2);
+
+// Updating
+const updated = numbers.map(n => n === 2 ? 20 : n);
 console.log(updated);
-
-const spread = {...person, name: "Bobby"}; // SPREAD OPERATOR SYNTAX
-const objectAssign = Object.assign({}, person, { name: "Bob", age: 30 }); // OBJECT.ASSIGN
-console.log(spread);
-console.log(objectAssign);
