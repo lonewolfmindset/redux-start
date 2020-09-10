d812927ac42d1ae88d3f0b2deebf91cfa5b18f62
lonewolfmindset <lonewolfmindset7@outlook.com>
2020-09-10 10:09:13 -0400

    Private properties

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import store from "./store";
-import { bugAdded, bugResolved } from "./actions";
-
-store.dispatch(bugAdded("Bug 1"));
-store.dispatch(bugResolved(1));
-
-console.log(store.getState());
+import store from "./customStore";
+store.state = 1;
+console.log(store);
