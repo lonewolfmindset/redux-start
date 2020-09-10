46857d88792bcc71776e7cba73fe020e7837f170
lonewolfmindset <lonewolfmindset7@outlook.com>
2020-09-10 11:40:32 -0400

    Dispatching actions

--- src/customStore.js
+++ src/customStore.js
@@ -1,13 +1,21 @@
-function createStore() {
+import reducer from "./reducer";
+
+function createStore(reducer) {
   let state;
+
+  function dispatch(action) {
+    // Call the reducer to get the new state
+    state = reducer(state, action);
+  }
 
   function getState() {
     return state;
   }
 
   return {
+    dispatch,
     getState,
   };
 }
 
-export default createStore();
+export default createStore(reducer);
