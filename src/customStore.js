e9207b2963a3f561dde1091c4f45294dd9e4c36d
lonewolfmindset <lonewolfmindset7@outlook.com>
2020-09-10 11:54:46 -0400

    Subscribing to the store

--- src/customStore.js
+++ src/customStore.js
@@ -2,10 +2,17 @@ import reducer from "./reducer";
 
 function createStore(reducer) {
   let state;
+  let listeners = [];
+
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
 
   function dispatch(action) {
     // Call the reducer to get the new state
     state = reducer(state, action);
+
+    for (let i = 0; i < listeners.length; i++) listeners[i]();
   }
 
   function getState() {
@@ -13,6 +20,7 @@ function createStore(reducer) {
   }
 
   return {
+    subscribe,
     dispatch,
     getState,
   };
