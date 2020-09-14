8e2513b5bdeab1f2fee92d2050ef12ca580ae554
lonewolfmindset <lonewolfmindset7@outlook.com>
2020-09-14 15:28:47 -0400

    Parameterizing Middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,6 +5,6 @@ import logger from "./middleware/logger";
 export default function () {
   return configureStore({
     reducer: reducer,
-    middleware: [logger],
+    middleware: [logger({ destination: "console" })],
   });
 }
