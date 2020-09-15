7358c17d45845676f23d9e59ffe528714ed2b986
lonewolfmindset <lonewolfmindset7@outlook.com>
2020-09-15 09:49:53 -0400

    Assigning a bug to a user

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -26,7 +26,7 @@ const slice = createSlice({
     },
 
     bugAssignedToUser: (bugs, action) => {
-      const { bugId, userId } = action.payload;
+      const { id: bugId, userId } = action.payload;
       const index = bugs.list.findIndex((bug) => bug.id === bugId);
       bugs.list[index].userId = userId;
     },
@@ -91,6 +91,14 @@ export const resolveBug = (id) =>
     method: "patch",
     data: { resolved: true },
     onSuccess: bugResolved.type,
+  });
+
+export const assignBugToUser = (bugId, userId) =>
+  apiRequest({
+    url: url + "/" + bugId,
+    method: "patch",
+    data: { userId },
+    onSuccess: bugAssignedToUser.type,
   });
 
 // Selector
